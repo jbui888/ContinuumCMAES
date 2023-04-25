b3800853651054caf16b602c0d91409eb3bc84e9
jbui888 <37464454+jbui888@users.noreply.github.com>
2023-04-25 12:13:58 -0700

    Update connect_comsol_server.m

--- connect_comsol_server.m
+++ connect_comsol_server.m
@@ -2,12 +2,16 @@
 % Connects MATLAB Livelink to COMSOL server
 % 
 %
-%
-% Date: April 24, 2023
-% Kaitlin Corpus 
+%    Date: Apr 25, 2023
 % 
+% (c) Kaitlin Corpus, LBNL. 
 %
-%==========================================================================
+% This code is licensed under a
+% MIT License.
+% You should have received a copy of the license along with this
+% work. If not, see <https://opensource.org/license/mit/>.
+% =========================================================================
+
 
 % Initiate COMSOL Server settings object
 comsol_settings = ComsolSettings();
